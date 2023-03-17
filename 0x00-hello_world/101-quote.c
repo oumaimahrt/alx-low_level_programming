60636cd62aabd6965e60f440bce07db45e732bf8
oumaimahrt <oumaimaahrite@gmail.com>
2023-03-17 07:24:43 -0700

    stdout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to standard error
+ * main - A c program that prints a line to standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
+return (1);
 }
