53a0070caf28806d470164935da210a924fe64ce
oumaimahrt <oumaimaahrite@gmail.com>
2023-03-23 16:50:03 -0700

    check if + or -

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,13 +2,15 @@
 /**
  * positive_or_negative - tests if num is + or -
  * @i: the num to be tested
- * Return: void
+ * Return: always 0
  */
 
 void positive_or_negative(int i)
 {
 	if (i < 0)
 		printf("%d is negative\n", i);
-	else
+	else if (i > 0)
 		printf("%d is positive\n", i);
+	else
+		printf("%d is zero\n", i);
 }
